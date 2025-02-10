f16dec8a45df294c2eb75a7e4c531cda2315c604
Paulo H B Souza <42076064+paulohbsouza@users.noreply.github.com>
2025-02-10 00:12:54 -0300

    start

---
+++ proj-interf-react/src/App.jsx
@@ -0,0 +1,13 @@
+//import React from "react";
+import { Button } from 'reactstrap';
+
+const App = () => {
+  return (
+    <>
+        <div>oi</div>
+        <Button>df</Button>
+    </>
+  )
+}
+
+export default App;
